c6ad68f8f073313cd585a3e2e230e1f125b62723
Wyler Patrick <patrick_wyler@hotmail.com>
2022-05-05 12:40:17 +0200

    setup workflow

---
+++ README.md
@@ -0,0 +1,8 @@
+# Helium miner stats
+
+## Chart
+![Chart](chart.png)
+
+### Features:
+- Get daily rewards of your miner and save them into /data directory (Github actions)
+- Generate a weekly updated chart out of this data (Github actions)
---
+++ src/main/java/ch/helium/UpdateChart.java
@@ -0,0 +1,111 @@
+package ch.helium;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.time.Month;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+
+import com.google.gson.Gson;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class UpdateChart {
+
+    public static void main(final String[] args) throws IOException {
+        LOG.info("Update bar chart image");
+
+        final File dataDir = Paths.get("data").toFile();
+        final File[] files = dataDir.listFiles();
+
+        final TimeSeries timeSeries = new TimeSeries("Rewards per month", "Month", "Rewards");
+
+        final Gson gson = new Gson();
+        final HashMap<String, Float> rewardsPerMonth = new HashMap<>();
+        Arrays.stream(files).forEach(file -> {
+            try {
+                final DataEntry dataEntry = gson.fromJson(new FileReader(file), DataEntry.class);
+                final float rewards = rewardsPerMonth.getOrDefault(getYearAndMonth(file), 0f) + dataEntry.getData().getTotal();
+                rewardsPerMonth.put(getYearAndMonth(file), rewards);
+            } catch (final Exception e) {
+                LOG.warn(e.getMessage());
+            }
+        });
+
+        final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM");
+
+        rewardsPerMonth.forEach((key, value) -> {
+            try {
+                timeSeries.add(new Month(formatter.parse(key)), value);
+            } catch (final ParseException e) {
+                LOG.warn(e.getMessage());
+            }
+        });
+
+        final JFreeChart barChart = ChartFactory.createXYBarChart("Helium Miner Rewards",
+                "Month",
+                true,
+                "HNT Rewards",
+                new TimeSeriesCollection(timeSeries),
+                PlotOrientation.VERTICAL,
+                true,
+                false,
+                false);
+        final int width = 1000;
+        final int height = 500;
+        final File outputFile = new File("chart.png");
+        ChartUtils.saveChartAsPNG(outputFile, barChart, width, height);
+    }
+
+    private static String getYearAndMonth(final File file) {
+        return file.getName().subSequence(0, 7).toString();
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class DataEntry {
+        Meta meta;
+        Data data;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Data {
+        private float total;
+        private float sum;
+        private float stddev;
+        private float min;
+        private float median;
+        private float max;
+        private float avg;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Meta {
+        private String min_time;
+        private String max_time;
+    }
+}
